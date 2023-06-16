3296128140670c573ffdcd3483167aa3cd3d053f
azeezridwan <azeezridwan803@gmail.com>
2023-06-16 09:14:36 -0700

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+include <stdio.h>
+/**
+ * main - Programming is like building a multilingual puzzle
+ * * Return: 0 (Success the program return)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
